91af0c4879fd00e538ba92fa2c5c2163aec4b329
Jared Gleisner <darkphoton31@gmail.com>
2025-04-05 19:28:39 -0500

    Fixed compatibility

--- server.py
+++ server.py
@@ -5,5 +5,5 @@ import ssl
 # serve with https
 
 if __name__ == "__main__":
-    http_server = WSGIServer(('0.0.0.0', 80), app)
+    http_server = WSGIServer(('0.0.0.0', 8080), app)
     http_server.serve_forever()
